25dc834594817dc0da67f30ce91a5da46b795687
toyinfa2884 <toyinfatai@gmail.com>
2021-11-24 11:03:18 +0100

    exmples

---
+++ StreamReduce.java
@@ -0,0 +1,9 @@
+package chapter17;
+
+import java.util.stream.IntStream;
+
+public class StreamReduce {
+    public static void main(String[] args) {
+        System.out.printf("Sum of 1 through 10 is: %d%n", IntStream.rangeClosed( 1, 10).sum());
+    }
+}
